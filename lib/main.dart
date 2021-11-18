260967b7501649b3683390ef1deedd275ec4e527
Daniel dos Santos <daniel@automastersat.com.br>
2021-11-18 15:18:23 -0300

    parte04 - finalizando a implementação do ListView.builder()

--- lib/main.dart
+++ lib/main.dart
@@ -82,19 +82,20 @@ class Editor extends StatelessWidget {
 }
 
 class ListaTransferencias extends StatelessWidget {
+  final List<Transferencia> _transferencias = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Transferências'),
       ),
-      body: Column(
-        children: [
-          ItemTransferencia(Transferencia(100.0, 1000)),
-          ItemTransferencia(Transferencia(200.0, 2000)),
-          ItemTransferencia(Transferencia(300.0, 3000)),
-        ],
-      ),
+      body: ListView.builder(
+          itemCount: _transferencias.length,
+          itemBuilder: (context, indice) {
+            final transferencia = _transferencias[indice];
+            return ItemTransferencia(transferencia);
+          }),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
@@ -106,6 +107,9 @@ class ListaTransferencias extends StatelessWidget {
           future.then((transferenciRecebida) {
             debugPrint('Chegou no then do future');
             debugPrint('$transferenciRecebida');
+            if (transferenciRecebida != null) {
+              _transferencias.add(transferenciRecebida);
+            }
             ScaffoldMessenger.of(context).showSnackBar(
               SnackBar(
                 content: Text('$transferenciRecebida'),
